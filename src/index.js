cde643a72b346d03e2e3a1d9a12fe8aba625f95d
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-03-15 16:12:19 -0300

    feat: using http methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,44 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world!' });
-})
+app.get('/courses', (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ]);
+});
+
+app.post('/courses', (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ]);
+});
+
+app.put('/courses/:id', (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ]);
+});
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ]);
+});
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ]);
+});
 
 app.listen(3333);
