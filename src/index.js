4fb293d86dcc33ced352d4d152160cae2ba7694a
Camilla Correia <camillajesuscorreia@hotmail.com>
2022-03-15 16:28:58 -0300

    feat: params types

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/courses', (request, response) => {
+  const query = request.query;
+  console.log(query);
+
   return response.json([
     "Curso 1",
     "Curso 2",
@@ -11,6 +16,9 @@ app.get('/courses', (request, response) => {
 });
 
 app.post('/courses', (request, response) => {
+  const body = request.body;
+  console.log(body);
+
   return response.json([
     "Curso 1",
     "Curso 2",
@@ -19,11 +27,9 @@ app.post('/courses', (request, response) => {
 });
 
 app.put('/courses/:id', (request, response) => {
-  return response.json([
-    "Curso 1",
-    "Curso 2",
-    "Curso 3"
-  ]);
+  const { id } = request.params;
+
+  return response.json(id);
 });
 
 app.patch('/courses/:id', (request, response) => {
